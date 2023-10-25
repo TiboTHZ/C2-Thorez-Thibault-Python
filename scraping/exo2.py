1aec936456a01ec99848df487ecbf98043293d04
TiboTHZ <t.thorez59@gmail.com>
2023-10-25 11:34:38 +0200

    Add: scrap exo2.2

--- scraping/exo2.py
+++ scraping/exo2.py
@@ -20,34 +20,34 @@ while i < 4 :
         print(f'{name}-{year}-{percent}')
     i = i+1
 
-# j = 1
-# while j < 11 :
-#     url = f"https://www.scrapethissite.com/pages/forms/?page_num={j}"
-#     page = requests.get(url)
+j = 1
+while j < 11 :
+    url = f"https://www.scrapethissite.com/pages/forms/?page_num={j}"
+    page = requests.get(url)
 
-#     soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-#     elements = soup.find_all(class_='team')
-#     entete = ["Nom", "Année", "Victoire", "Défaite", "OT loose", "Pourcentage victoire", "But pour", "But contre", "+/-"]
-#     with open ('data.csv', "w") as fichier_csv:
-#         writer = csv.writer[fichier_csv]
-#         writer.writeRow(entete)
-#         for element in elements:
-#             try:
-#                 diff = element.find('td', class_='diff text-success').text.strip()
-#                 name = element.find('td', class_='name').text.strip()
-#                 year = element.find('td', class_='year').text.strip()
-#                 try:
-#                     percent = element.find('td', class_='pct text-success').text.strip()
-#                 except:
-#                     percent = element.find('td', class_='pct text-danger').text.strip()
-#                 win = element.find('td', class_='wins').text.strip()
-#                 loose = element.find('td', class_='losses').text.strip()
-#                 ot = element.find('td', class_='ot-losses').text.strip()
-#                 ga = element.find('td', class_='ga').text.strip()
-#                 gf = element.find('td', class_='gf').text.strip()
-#                 entete = [name, year, win, loose, ot, percent, gf, ga, diff]
-
-#             except:
-#                 print()
-#     j = j+1
+    elements = soup.find_all(class_='team')
+    entete = ["Nom", "Année", "Victoire", "Défaite", "OT loose", "Pourcentage victoire", "But pour", "But contre", "+/-"]
+    with open ('data.csv', "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(entete)
+        for element in elements:
+            try:
+                diff = element.find('td', class_='diff text-success').text.strip()
+                name = element.find('td', class_='name').text.strip()
+                year = element.find('td', class_='year').text.strip()
+                try:
+                    percent = element.find('td', class_='pct text-success').text.strip()
+                except:
+                    percent = element.find('td', class_='pct text-danger').text.strip()
+                win = element.find('td', class_='wins').text.strip()
+                loose = element.find('td', class_='losses').text.strip()
+                ot = element.find('td', class_='ot-losses').text.strip()
+                ga = element.find('td', class_='ga').text.strip()
+                gf = element.find('td', class_='gf').text.strip()
+                row = [name, year, win, loose, ot, percent, gf, ga, diff]
+                writer.writerow(row)
+            except:
+                print()
+    j = j+1
